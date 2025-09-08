6f4f60d64ac157a6aca1027598242da28826e98d
Mdj3530 <vynceianoani@gmail.com>
2025-09-08 12:15:22 +0800

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ with open("label_encoder.pkl", "rb") as f:
     label_encoder = pickle.load(f)
 
 # 🔥 Load the unified SK FAQ dataset
-faq_df = pd.read_csv("sk_faq_dataset_full.csv")
+faq_df = pd.read_csv("sk_faq_dataset_intents.csv")
 
 max_len = 20
 
